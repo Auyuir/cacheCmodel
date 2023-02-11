10e223df4b6e4ce4cfd411b5cc09bfa5165edfc8
au@109WSLU20 <auchufeng@qq.com>
2023-02-11 17:04:49 +0800

    split l1_data_cache.h file

---
+++ l1_data_cache.h
@@ -0,0 +1,273 @@
+#include "parameter.h"
+#include "tag_array.h"
+#include "miss_status_holding_reg.h"
+#include "interfaces.h"
+
+class l1_data_cache : public cache_building_block{
+public:
+    l1_data_cache(){
+        m_coreReq_ptr = nullptr;
+        m_coreReq_pipe1_reg_ptr = nullptr;
+        m_coreRsp_pipe2_reg_ptr = nullptr;
+        m_memRsp_pipe1_reg_ptr = nullptr;
+        m_tag_array = tag_array(m_memReq_Q);
+        m_mshr = mshr(m_memReq_Q, m_tag_array);
+    }
+
+    void coreReq_pipe1_cycle();
+
+    //cache主体周期，产生道路分歧
+    void coreReq_pipe2_cycle(cycle_t time);
+
+    //coreReq-coreRsp hit时，从data SRAM到coreRsp_Q
+    void coreReq_pipe3_cycle();
+
+    void memRsp_pipe1_cycle();
+
+    void memRsp_pipe2_cycle(cycle_t time);
+
+    void coreRsp_Q_cycle();
+
+    void memReq_Q_cycle();
+
+    void cycle(cycle_t time);
+
+    void cast_amo_LSU_type_2_TLUH_param(enum LSU_cache_coreReq_type_amo coreReq_type, 
+    enum TL_UH_A_opcode& TL_opcode, u_int32_t& TL_param){
+        //TODO
+        switch(coreReq_type)
+        {
+            case amoadd:
+                TL_opcode = ArithmeticData;
+                TL_param = u_int32_t(ADD);
+                break;
+            case amoxor:
+                TL_opcode = LogicalData;
+                TL_param = u_int32_t(XOR);
+                break;
+            case amoand:
+                TL_opcode = LogicalData;
+                TL_param = u_int32_t(AND);
+                break;
+            case amoor:
+                TL_opcode = LogicalData;
+                TL_param = u_int32_t(OR);
+                break;
+            case amomin:
+                TL_opcode = ArithmeticData;
+                TL_param = u_int32_t(MIN);
+                break;
+            case amomax:
+                TL_opcode = ArithmeticData;
+                TL_param = u_int32_t(MAX);
+                break;
+            case amominu:
+                TL_opcode = ArithmeticData;
+                TL_param = u_int32_t(MINU);
+                break;
+            case amomaxu:
+                TL_opcode = ArithmeticData;
+                TL_param = u_int32_t(MAXU);
+                break;
+            case amoswap:
+                TL_opcode = LogicalData;
+                TL_param = u_int32_t(SWAP);
+                break;
+        }
+    }
+
+public:
+    LSU_2_dcache_coreReq* m_coreReq_ptr;
+    LSU_2_dcache_coreReq* m_coreReq_pipe1_reg_ptr;//pipe1和2之间的流水线寄存器
+    //TODO:[初版建模完成后]m_coreReq_pipe1_reg_ptr不用和coreReq相同的类型，而是定制化
+    dcache_2_LSU_coreRsp* m_coreRsp_pipe2_reg_ptr;//read hit 路径/write miss路径/missRsp路径
+    //该寄存器由l1_data_cache的memRsp_pipe1_cycle检查和置1，由memRsp_pipe2_cycle置0。
+    //如果用SRAM实现MSHR，硬件中没有这个寄存器，用SRAM的保持功能实现相应功能。
+    //如果用reg实现MSHR，可以按照本模型行为设计寄存器。
+    mshr_miss_rsp* m_memRsp_pipe1_reg_ptr;
+    bool m_coreRsp_ready;//每个周期可由tb改变的信号，代表当前周期是否可以传输coreRsp
+    bool m_memReq_ready;//每个周期可由tb改变的信号，代表当前周期是否可以传输memReq
+
+    tag_array m_tag_array;
+    mshr m_mshr;
+    coreRsp_Q m_coreRsp_Q;
+    memReq_Q m_memReq_Q;
+    memRsp_Q m_memRsp_Q;
+};
+
+void l1_data_cache::coreReq_pipe1_cycle(){
+    if(m_memRsp_Q.m_Q.size() == 0){
+        if(m_coreReq_ptr != nullptr){
+            if(m_coreReq_pipe1_reg_ptr == nullptr){
+                auto const coreReq_opcode = m_coreReq_ptr->m_opcode;
+                if (coreReq_opcode==Read || coreReq_opcode==Write || coreReq_opcode==Amo){
+                    if(m_coreReq_ptr->m_type == 1 || coreReq_opcode==Amo){//LR/SC
+                        //发起对speMSHR可用性的检查
+                    }else{//regular R/W
+                        //发起tag probe
+                        //同步发起vecMSHR probe
+                    }
+                    m_coreReq_ptr = nullptr;
+                }
+            }
+        }
+    }
+}
+
+void l1_data_cache::coreReq_pipe2_cycle(cycle_t time){
+    auto& pipe1_r_ptr = m_coreReq_pipe1_reg_ptr;
+    if(pipe1_r_ptr != nullptr){
+        auto const pipe1_opcode = pipe1_r_ptr->m_opcode;
+        auto const pipe1_block_idx = pipe1_r_ptr->m_block_idx;
+        if (pipe1_opcode==Read || pipe1_opcode==Write || pipe1_opcode==Amo){
+            if(pipe1_r_ptr->m_type == 1 ||//LR/SC
+            pipe1_opcode==Amo){
+                //实际硬件行为中，mshr的probe发生在pipe1_cycle，结果在pipe2_cycle取得。
+                if (m_mshr.probe_spe() == AVAIL){
+                    if (!m_memReq_Q.is_full()){
+                        enum entry_target_type new_spe_type;
+                        enum TL_UH_A_opcode new_mReq_opcode;
+                        u_int32_t new_mReq_param;
+                        //push spe MSHR
+                        if(pipe1_opcode==Amo){
+                            new_spe_type = AMO;
+                            cast_amo_LSU_type_2_TLUH_param(pipe1_r_ptr->m_amo_type,
+                                new_mReq_opcode,new_mReq_param);
+                        }
+                        else if (pipe1_opcode == Read){
+                            new_spe_type = LOAD_RESRV;
+                            new_mReq_opcode = Get;
+                            new_mReq_param = 0x1;
+                        }
+                        else{
+                            new_spe_type = STORE_COND;
+                            new_mReq_opcode = PutFullData;
+                            new_mReq_param = 0x1;
+                        }
+                        m_mshr.allocate_special(new_spe_type, 
+                            pipe1_r_ptr->m_reg_idxw, pipe1_r_ptr->m_wid);
+                        //push memReq_Q
+                        dcache_2_L2_memReq new_spe_req = dcache_2_L2_memReq(
+                            new_mReq_opcode, new_mReq_param, 
+                            pipe1_r_ptr->m_reg_idxw, pipe1_block_idx);
+                        m_memReq_Q.m_Q.push_back(new_spe_req);
+
+                        pipe1_r_ptr = nullptr;
+                    }
+                }
+            }else{//regular R/W
+                u_int32_t way_idx=0;//hit情况下所在的way
+                //实际硬件行为中，tag的probe发生在pipe1_cycle，结果在pipe2_cycle取得。
+                enum tag_access_status status = 
+                    m_tag_array.probe(pipe1_block_idx,way_idx);
+                if (status == HIT){
+                    if(!m_coreRsp_Q.is_full()){
+                        m_tag_array.read_hit_update_access_time(pipe1_block_idx,way_idx,time);
+                        assert(m_coreRsp_pipe2_reg_ptr==nullptr);
+                        //arrange coreRsp
+                        //本模型不建模访问data SRAM行为，在此处对该SRAM发起访问，
+                        dcache_2_LSU_coreRsp read_hit_coreRsp(pipe1_r_ptr->m_reg_idxw,
+                            true,pipe1_r_ptr->m_wid,pipe1_r_ptr->m_mask);
+                        m_coreRsp_pipe2_reg_ptr = &read_hit_coreRsp;//TODO:有内存管理问题吗
+                        pipe1_r_ptr = nullptr;
+                    }
+                }else{//status == MISS
+                    if(pipe1_opcode==Read){
+                        //实际硬件行为中，mshr的probe发生在pipe1_cycle，结果在pipe2_cycle取得。
+                        enum vec_mshr_status mshr_status = m_mshr.probe_vec(pipe1_block_idx);
+                        if (mshr_status == PRIMARY_AVAIL){
+                            if (!m_memReq_Q.is_full()){
+                                //vecMSHR记录新entry
+                                vec_subentry new_vec_sub = vec_subentry(
+                                    pipe1_r_ptr->m_reg_idxw, pipe1_r_ptr->m_wid, pipe1_r_ptr->m_mask);
+                                m_mshr.allocate_vec_main(pipe1_block_idx, new_vec_sub);
+                                //push memReq Q
+                                dcache_2_L2_memReq new_read_miss = dcache_2_L2_memReq(
+                                    Get, 0x0, pipe1_r_ptr->m_reg_idxw, pipe1_block_idx);
+                                m_memReq_Q.m_Q.push_back(new_read_miss);
+                                pipe1_r_ptr = nullptr;
+                            }
+                        }else if(mshr_status == SECONDARY_AVAIL){
+                            //vecMSHR在旧entry下记录新成员
+                            vec_subentry new_vec_sub = vec_subentry(
+                                pipe1_r_ptr->m_reg_idxw, pipe1_r_ptr->m_wid, pipe1_r_ptr->m_mask);
+                            m_mshr.allocate_vec_sub(pipe1_block_idx, new_vec_sub);
+                            pipe1_r_ptr = nullptr;
+                        }//PRIMARY_FULL和SECONDARY_FULL直接跳过
+                    }else{//Write (write no allocation when miss)
+                        if (!m_memReq_Q.is_full() && !m_coreRsp_Q.is_full()){
+                            //arrange coreRsp
+                            dcache_2_LSU_coreRsp read_hit_coreRsp(pipe1_r_ptr->m_reg_idxw,
+                                true,pipe1_r_ptr->m_wid,pipe1_r_ptr->m_mask);
+                            m_coreRsp_pipe2_reg_ptr = &read_hit_coreRsp;
+                            //push memReq Q
+                            dcache_2_L2_memReq new_write_miss = dcache_2_L2_memReq(
+                                PutFullData, 0x0, pipe1_r_ptr->m_reg_idxw, pipe1_block_idx);
+                            m_memReq_Q.m_Q.push_back(new_write_miss);
+                            pipe1_r_ptr = nullptr;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    //不清除pipe_r_ptr，下个周期再处理一回
+}
+
+void l1_data_cache::coreReq_pipe3_cycle(){
+    if(m_coreRsp_pipe2_reg_ptr != nullptr){
+        if(!m_coreRsp_Q.is_full()){
+            m_coreRsp_Q.m_Q.push_back(*m_coreRsp_pipe2_reg_ptr);
+            m_coreRsp_pipe2_reg_ptr == nullptr;
+        }
+    }
+}
+
+void l1_data_cache::memRsp_pipe1_cycle(){
+    if(m_memRsp_Q.m_Q.size() != 0){
+        if(m_memRsp_pipe1_reg_ptr == nullptr){
+            auto const req_id = m_memRsp_Q.m_Q.front().m_req_id;
+            block_addr_t block_idx;
+            auto missRsp_type = m_mshr.detect_missRsp_type(block_idx, req_id);
+            mshr_miss_rsp new_miss_rsp = mshr_miss_rsp(missRsp_type,req_id, block_idx);
+            m_memRsp_pipe1_reg_ptr = &new_miss_rsp;
+
+            m_memRsp_Q.m_Q.pop_front();
+        }
+    }
+}
+
+void l1_data_cache::memRsp_pipe2_cycle(cycle_t time){
+    if(m_memRsp_pipe1_reg_ptr != nullptr){
+        if(m_mshr.missRsp_process(*m_memRsp_pipe1_reg_ptr, time)){
+            m_memRsp_pipe1_reg_ptr = nullptr;
+        }
+    }
+}
+
+//需要tb先取出当前coreRsp里front的内容之后，再运行该函数
+void l1_data_cache::coreRsp_Q_cycle(){
+    if(!m_coreRsp_Q.is_empty() && m_coreRsp_ready){
+        m_coreRsp_Q.m_Q.pop_front();
+    }
+}
+
+//需要tb先取出当前memReq_Q里front的内容之后，再运行该函数
+void l1_data_cache::memReq_Q_cycle(){
+    if(!m_memReq_Q.is_empty() && m_memReq_ready){
+        m_memReq_Q.m_Q.pop_front();
+    }
+}
+
+void l1_data_cache::cycle(cycle_t time){
+    coreRsp_Q_cycle();
+    memReq_Q_cycle();
+
+    coreReq_pipe3_cycle();
+
+    memRsp_pipe2_cycle(time);//memRsp pipe2和coreReq pipe2之间没有相关
+    coreReq_pipe2_cycle(time);
+
+    coreReq_pipe1_cycle();//memRsp的优先级比coreReq高，coreReq pipe1必须在memRsp pipe1之间运行
+    memRsp_pipe1_cycle();//否则memRsp pipe1的运行会pop memRsp_Q
+}
